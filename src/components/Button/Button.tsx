b46b96bba91dcd395ac3148c0e0ed1d53975e276
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-07-18 13:50:29 +0200

    Add className prop to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import React, { ReactElement } from "react"
 type ButtonProps = {
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
     label: string,
+    className: string,
 }
 
-const Button = ({onClick, label}:ButtonProps): ReactElement => (
-    <button onClick={onClick}>
+const Button = ({onClick, label, className}:ButtonProps): ReactElement => (
+    <button className={`${className}`} onClick={onClick}>
         {label}
     </button>
 )
